c88d0031749989d1b68ed70ea8622e3a14bb6b86
Walrusking <walruskinggaming@gmail.com>
2023-10-12 16:08:56 -0400

    fixes favicon location and setups project for deploy

--- src/lib/components/SideNavbar.svelte
+++ src/lib/components/SideNavbar.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 
 	const routes = [
 		{
@@ -24,7 +25,7 @@
 				{#each routes as route}
 					{@const activeRoute = isActive(route)}
 					<li class="rounded group transition-colors {activeRoute ? 'bg-sky-700 text-white' : 'bg-sky-900 focus:bg-sky-700 hover:bg-sky-700 text-gray-300 focus:text-white hover:text-white'}">
-						<a href="{route.path}" class="block w-full h-full p-2">
+						<a href="{base}/{route.path}" class="block w-full h-full p-2">
 							<span class="{activeRoute ? 'ml-4' : 'transition-all group-focus:ml-4 group-hover:ml-4'}">{route.name}</span>
 						</a>
 					</li>
