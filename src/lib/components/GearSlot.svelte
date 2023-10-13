83cba2002491c12d9cd0ddfaf1bde40023d73462
Walrusking <walruskinggaming@gmail.com>
2023-10-13 00:25:10 -0400

    feat(gear-builder): remove more unused components

--- src/lib/components/GearSlot.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let item;
-</script>
-
-<div class="bg-black/70 border border-gray-500 w-20 h-20 flex items-center justify-center">
-	<img src={item.image} alt={item.name} width="100" height="100">
-</div>
